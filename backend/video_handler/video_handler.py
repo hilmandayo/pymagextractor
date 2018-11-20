b13ac1965acff0a041f20ff338396326e25e2f88
Hilman <hilman_94@icloud.com>
2018-11-20 16:56:41 +0900

    A working prototype of cv2 video threads.

--- backend/video_handler/video_handler.py
+++ backend/video_handler/video_handler.py
@@ -1,25 +1,36 @@
-from queue import Queue
+from collections import deque
 import threading
 
 import cv2
 
+from . import helper
+
 
 # TODO: Credit to the creator.
 # TODO: Test and compare this version with the normal one.
+<<<<<<< HEAD
 # TODO: REWRITE BACK EVERYTHING!
+=======
+# CONT: Try to exhaust test this framework
+>>>>>>> 3220f8e... A working prototype of cv2 threads.
 class VideoCaptureAsync:
     def __init__(self, src=0, width=None, height=None, queue_size=128):
         self.src = src
         self.cap = cv2.VideoCapture(self.src)
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-        self.Q = Queue(maxsize=queue_size)
+        self.Q = deque(maxlen=queue_size)
         # self.grabbed, self.frame = self.cap.read()
         self.started = False
         self.lock = threading.Lock()
 
-        self.Q_frame_start = None
-        self.Q_frame_end = 0
+        # This bookkeeping would be in asymmetric format.
+        self.Q_frame_start = 0
+        self.Q_frame_end = queue_size
+
+        # Dynamic variables.
+        self.Q_frame_head = 0
+        self.Q_frame_tail = 0
 
     # Not needed?
     def set(self, var1, var2):
@@ -34,39 +45,44 @@ class VideoCaptureAsync:
         self.thread.start()
         return self
 
-    def _refresh_queue(self, old_start, old_end):
-        if self.Q_frame_end >= old_end:
-            pass  # cont from here
+    def _refresh_deque(self, old_start, old_end):
+        # print("k")
+        helper._refresh_deque(self, old_start, old_end)
 
     def _update(self):
         old_start = self.Q_frame_start
         old_end = self.Q_frame_end
         while self.started:
             if old_start != self.Q_frame_start or old_end != self.Q_frame_end:
-                self._refresh_queue(old_start, old_end)
+                self._refresh_deque(old_start, old_end)
 
+                # print(old_start)
+                # print(old_end)
+                # print(self.Q_frame_start)
+                # print(self.Q_frame_end)
+                # print(self.Q_frame_tail)
+                # print(self.Q_frame_head)
                 old_start = self.Q_frame_start
                 old_end = self.Q_frame_end
 
-            if not self.Q.full():
+            if not len(self.Q) >= self.Q.maxlen:
                 grabbed, frame = self.cap.read()
                 if grabbed:
-                    self.Q.put(frame)
+                    print("I am here")
+                    self.Q.append(frame)
 
-                    if self.Q_frame_start is None:
-                        self.Q_frame_start = 0
-                    self.Q_frame_end += 1
+                    if self.Q_frame_head is None:
+                        self.Q_frame_head = 0
+                    self.Q_frame_tail += 1
+            else:
+                print(self.cap.get(cv2.CAP_PROP_POS_FRAMES))
 
-    def refresh_queue(self, start, end):
+    def refresh_deque(self, start, end):
         with self.lock:  # Is this needed?
             self.Q_frame_start = start
             self.Q_frame_end = end
-
-    def read(self):
-        with self.read_lock:
-            frame = self.frame.copy()
-            grabbed = self.grabbed
-        return grabbed, frame
+            self.Q_frame_head = start
+            self.Q_frame_tail = start
 
     def stop(self):
         self.started = False
@@ -77,10 +93,3 @@ class VideoCaptureAsync:
 
     def __exit__(self, exec_type, exc_value, traceback):
         self.cap.release()
-
-
-# Continue:
-# 1) Make sure how threading works
-# 2) Make a really good plan
-# 3) Check about lock
-# 4) Make sure of case too high of start and end
