6442874eab742214f73cf60902653d8ee532e70f
Mohd Hafiz Hilman <hilman_94@icloud.com>
2019-12-19 19:22:29 +0900

    Little update...

--- pymagextractor/models/data_handlers/__init__.py
+++ pymagextractor/models/data_handlers/__init__.py
@@ -4,3 +4,5 @@
 
 from .data_handler import DataHandler
 from .handler import Handler
+
+print("hai Handler")
--- pymagextractor/tests/func/test_handler.py
+++ pymagextractor/tests/func/test_handler.py
@@ -1,6 +1,16 @@
 from pymagextractor.models.data_handlers import Handler
 
 
-@pytest.parametrize
-def test_handler_inits():
-    pass
+@pytest.mark.parametrize("name, ref, button", [("Track ID", "track_id", None),
+                                               ("Frame ID", "frame_id", False),
+                                               ("X1", "x1", True),
+                                               ("Y1", "y1", True)])
+def test_handler_inits(name, ref, button):
+    if button is None:
+        handler = Handler(name, ref)
+        button = False
+    else:
+        handler = Handler(name, ref, button)
+
+    h = handler
+    assert (h.name, h.ref, h.button) == (name, ref, button)
